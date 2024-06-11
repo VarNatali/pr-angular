01bb3ab0e77e8a5a83eb3cd6832089155eb768f8
VarNatali <nataliiavaren@gmail.com>
2024-06-11 21:41:10 +0200

    lesson02

---
+++ src/app/components/my-component/my-component.component.ts
@@ -0,0 +1,9 @@
+
+
+@Component({
+  selector: 'app-my-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-component.component.html',
+  styleUrl: './my-component.component.scss'
+})
